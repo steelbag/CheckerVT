95a0c9049d42f92aa5338e8f9c7deff1d9aafc52
steelbag <steelbag01@gmail.com>
2018-01-09 15:36:38 +0900

    Try Move

--- Assets/Script/CheckersBoard.cs
+++ Assets/Script/CheckersBoard.cs
@@ -11,7 +11,12 @@ public class CheckersBoard : MonoBehaviour {
     private Vector3 boardOffset = new Vector3(-4.0f, 0, -4.0f);
     private Vector3 pieceOffset = new Vector3(0.5f, 0, 0.5f);
 
+    public bool isWhite;
+    private bool isWhiteTurn;
+    private bool hasKilled;
+
     private Piece selectedPiece;
+    private List<Piece> forcedPieces;
 
     private Vector2 mouseOver;
     private Vector2 startDrag;
@@ -20,6 +25,8 @@ public class CheckersBoard : MonoBehaviour {
     private void Start()
     {
         GenerateBoard();
+        forcedPieces = new List<Piece>();
+        isWhiteTurn = true;
     }
 
     private void Update()
@@ -83,27 +90,40 @@ public class CheckersBoard : MonoBehaviour {
     private void SelectPiece(int x, int y)
     {
         // Out of Bound
-        if (x < 0 || x >= pieces.Length || y < 0 || y >= pieces.Length)
+        if (x < 0 || x >= 8 || y < 0 || y >= 8)
             return;
 
         Piece p = pieces[x, y];
-        if(p != null)
+        if(p != null && p.isWhite == isWhite)
         {
-            selectedPiece = p;
-            startDrag = mouseOver;
-            Debug.Log(selectedPiece.name);
+            if(forcedPieces.Count == 0)
+            {
+                selectedPiece = p;
+                startDrag = mouseOver;
+            }
+            else
+            {
+                // Look for the piece under our forced pieces list
+                if (forcedPieces.Find(fp => fp == p) == null)
+                    return;
+
+                selectedPiece = p;
+                startDrag = mouseOver;
+            }
         }
     }
 
     private void TryMove(int x1, int y1, int x2, int y2)
     {
+        forcedPieces = ScanForPossibleMove();
+
         // Multiplayer support
         startDrag = new Vector2(x1, y1);
         endDrag = new Vector2(x2, y2);
         selectedPiece = pieces[x1, y1];
 
         // Check if we are out of bound
-        if(x2 < 0 || x2 >= pieces.Length || y2 < 0 || y2 >= pieces.Length)
+        if(x2 < 0 || x2 >= 8 || y2 < 0 || y2 >= 8)
         {
             if (selectedPiece != null)
                 MovePiece(selectedPiece, x1, y1);
@@ -124,7 +144,73 @@ public class CheckersBoard : MonoBehaviour {
             }
 
             // Check if its a vaild move
+            if(selectedPiece.VaildMove(pieces, x1, y1, x2, y2))
+            {
+                // Did we kill anything
+                // If this is a jump
+                if(Mathf.Abs(x2 - x1) == 2)
+                {
+                    Piece p = pieces[(x1 + x2) / 2, (y1 + y2) / 2];
+                    if(p != null)
+                    {
+                        pieces[(x1 + x2) / 2, (y1 + y2) / 2] = null;
+                        Destroy(p.gameObject);
+                        hasKilled = true;
+                    }
+                }
+
+                // were we suposed to kill anything?
+                if(forcedPieces.Count != 0 && !hasKilled)
+                {
+                    MovePiece(selectedPiece, x1, y1);
+                    startDrag = Vector2.zero;
+                    selectedPiece = null;
+                    return;
+                }
+
+                pieces[x2, y2] = selectedPiece;
+                pieces[x1, y1] = null;
+                MovePiece(selectedPiece, x2, y2);
+
+                EndTurn();
+            }
+            else
+            {
+                MovePiece(selectedPiece, x1, y1);
+                startDrag = Vector2.zero;
+                selectedPiece = null;
+                return;
+            }
         }
+    }
+
+    private void EndTurn()
+    {
+        selectedPiece = null;
+        startDrag = Vector2.zero;
+
+        isWhiteTurn = !isWhiteTurn;
+        isWhite = !isWhite;
+        hasKilled = false;
+        CheckVictory();
+    }
+
+    private void CheckVictory()
+    {
+
+    }
+
+    private List<Piece> ScanForPossibleMove()
+    {
+        forcedPieces = new List<Piece>();
+
+        // Check all the pieces
+        for (int i = 0; i < 8; i++)
+            for (int j = 0; j < 8; j++)
+                if (pieces[i, j] != null && pieces[i, j].isWhite == isWhiteTurn)
+                    if (pieces[i, j].IsForceToMove(pieces, i, j))
+                        forcedPieces.Add(pieces[i, j]);
+        return forcedPieces;
     }
 
     private void GenerateBoard()
--- Assets/Script/Piece.cs
+++ Assets/Script/Piece.cs
@@ -7,6 +7,65 @@ public class Piece : MonoBehaviour {
     public bool isWhite;
     public bool isKing;
 
+    public bool IsForceToMove(Piece[,] board, int x, int y)
+    {
+        if(isWhite || isKing)
+        {
+            // Top Left
+            if(x >= 2 && y <= 5)
+            {
+                Piece p = board[x - 1, y + 1];
+                // If there is a piece, and it is not the same color as ours
+                if (p != null && p.isWhite != isWhite)
+                {
+                    // Check if its possible to land after the jump
+                    if (board[x - 2, y + 2])
+                        return true;
+                }
+            }
+            // Top Right
+            if (x <= 5 && y <= 5)
+            {
+                Piece p = board[x + 1, y + 1];
+                // If there is a piece, and it is not the same color as ours
+                if (p != null && p.isWhite != isWhite)
+                {
+                    // Check if its possible to land after the jump
+                    if (board[x + 2, y + 2])
+                        return true;
+                }
+            }
+        }
+
+        if (!isWhite || isKing)
+        {
+            // Bottom Left
+            if (x >= 2 && y >= 2)
+            {
+                Piece p = board[x - 1, y - 1];
+                // If there is a piece, and it is not the same color as ours
+                if (p != null && p.isWhite != isWhite)
+                {
+                    // Check if its possible to land after the jump
+                    if (board[x - 2, y - 2])
+                        return true;
+                }
+            }
+            // Bottom Right
+            if (x <= 5 && y >= 2)
+            {
+                Piece p = board[x + 1, y - 1];
+                // If there is a piece, and it is not the same color as ours
+                if (p != null && p.isWhite != isWhite)
+                {
+                    // Check if its possible to land after the jump
+                    if (board[x + 2, y - 2])
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
 	public bool VaildMove(Piece[,] board, int x1, int y1, int x2, int y2)
     {
         // If you are moving on top of another piece
@@ -14,7 +73,7 @@ public class Piece : MonoBehaviour {
             return false;
 
         int deltaMove = Mathf.Abs(x1 - x2);
-        int deltaMoveY = y1 - y2;
+        int deltaMoveY = y2 - y1;
         if(isWhite || isKing)
         {
             if(deltaMove == 1)
